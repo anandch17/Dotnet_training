54c792edbce81ff6813260003f699a0e2b6c9711
anandch17 <160841358+anandch17@users.noreply.github.com>
2026-02-06 23:11:44 +0530

    Custom Exception Handling with bank amount withdraw function

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           customer c=new customer("Anand",1234,400);
+            Console.WriteLine(c);
+
+
+            try
+            {
+                c.withdraw(350);
+            }
+            catch(bankexception e)
+            {
+                Console.WriteLine("transaction failed");
+                e.inform();
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+	public class customer
+	{
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		private int _balance;
+
+		public int Balance
+		{
+			get { return _balance; }
+			set { _balance = value; }
+		}
+
+		private int _accno;
+		public int AccNo
+		{
+			get { return _accno; }
+			set { _accno = value; }
+		}
+
+
+
+		public customer()
+		{
+
+		}
+
+
+		public customer(string name,int acc,int bal)
+		{
+			_name = name;
+			_accno = acc;
+			_balance = bal;
+		}
+		 public void withdraw(int amount)
+		{
+			if (_balance - amount <= 100)
+			{
+				throw new bankexception(_accno,_balance);
+			}
+			_balance -= amount;
+		}
+
+		public int getbalance() { return _balance; }
+
+        public override string ToString()
+        {
+			return $"Account Holders is {_name}" + $" Account no is {_accno} "+ $"Balance:{_balance}";
+        }
+
+	}
+}
